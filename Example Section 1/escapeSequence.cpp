29005b92fd96b64b34209e33a21d9370f70fe566
bbdoggett3 <37028720+bbdoggett3@users.noreply.github.com>
2020-02-26 23:15:40 -0700

    Create escapeSequence.cpp

---
+++ Example Section 1/escapeSequence.cpp
@@ -0,0 +1,26 @@
+  /***********************************************
+  * ESCAPE SEQUENCE
+  * Author: Benjamin Doggett
+  *
+  * I will be creating a program that will display 
+  * to the user all the escape cases that they can
+  * use while programming in C++
+  ************************************************/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+   //Here is where I create my escape sequence cases.
+   cout << "The escape sequences are: "      << endl;
+   cout << "\t Newline" << setw(5) << "\\n"  << endl;
+   cout << "\t Tab"     << setw(9) << "\\t"  << endl;
+   cout << "\t Slash"   << setw(7) << "\\\\" << endl;
+   cout << "\t SQuote"  << setw(6) << "\\\'" << endl;
+   cout << "\t DQuote"  << setw(6) << "\\\"" << endl;
+
+   return 0;
+}
